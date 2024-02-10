3130569a342e8a1806a44524f64a8a6dfc931746
Arthur Teixeira <arthursony1@hotmail.com>
2024-02-09 21:38:24 -0300

    fix path req1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    sal = Ingredient("sal")
+    sal2 = Ingredient("sal")
+    presunto = Ingredient("presunto")
+
+    assert sal.__hash__() == sal2.__hash__()
+    assert sal.__hash__() != presunto.__hash__()
+
+    assert sal.__eq__(sal2) is True
+    assert sal.__eq__(presunto) is False
+
+    assert sal.name == "sal"
+
+    assert presunto.restrictions == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT}
+
+    assert sal.__repr__() == "Ingredient('sal')"
+
